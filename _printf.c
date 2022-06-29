72ad4bb10def4d7887e20d81a212fadc3c945a90
nathanhall762 <4943@holbertonschool.com>
2022-06-29 15:18:36 -0700

    init: main.c and _prinf

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _printf - produces output according to a format
+* @format: format string
+*
+* Return: the number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	
+}
