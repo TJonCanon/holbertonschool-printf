1dd9ffee3012983d98fc40aa60c2cf32b1823048
nathanhall762 <4943@holbertonschool.com>
2022-07-01 13:45:05 -0700

    fix: printf with switch case works

--- _printf.c
+++ _printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdarg.h>
+#include <string.h>
 
 /**
 * _printf - produces output according to a format
@@ -9,5 +11,51 @@
 
 int _printf(const char *format, ...)
 {
-	
+	va_list lst;
+	int i = 0, j = 0;
+	char buff[100] = {0};
+	char *str_arg;
+
+	va_start(lst, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					buff[j] = (char)va_arg(lst, int);
+					j++;
+					break;
+				case 's':
+					str_arg = va_arg(lst, char *);
+					_strncpy(&buff[j], str_arg);
+					j += _strlen(str_arg);
+					break;
+				case '%':
+					buff[j] = '%';
+					j++;
+					break;
+				default:
+					buff[j] = '%';
+					j++;
+					buff[j] = format[i];
+					continue;
+			}
+		}
+		else
+		{
+			buff[j] = format[i];
+			j++;
+		}
+		i++;
+	}
+	for (i = 0; i <= j; i++)
+	{
+		_putchar(buff[i]);
+	}
+	va_end(lst);
+	return (j);
 }
--- main.h
+++ main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+void print_char(char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src);
+int _putchar(char c);
 
 #endif /* MAIN_H */
